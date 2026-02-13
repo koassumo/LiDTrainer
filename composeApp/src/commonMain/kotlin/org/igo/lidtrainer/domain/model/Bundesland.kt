3cc5a887a0aeb7e5592346ffdff1ebdce0b691d9
koassumo <0770407@gmail.com>
2026-02-13 16:52:45 +0100

    jsons

---
+++ composeApp/src/commonMain/kotlin/org/igo/lidtrainer/domain/model/Bundesland.kt
@@ -0,0 +1,20 @@
+package org.igo.lidtrainer.domain.model
+
+enum class Bundesland(val displayName: String) {
+    BADEN("Baden-Württemberg"),
+    BAYERN("Bayern"),
+    BERLIN("Berlin"),
+    BRANDENBURG("Brandenburg"),
+    BREMEN("Bremen"),
+    HAMBURG("Hamburg"),
+    HESSEN("Hessen"),
+    MECKLENBURG("Mecklenburg-Vorpommern"),
+    NIEDERSACHSEN("Niedersachsen"),
+    NORDRHEIN("Nordrhein-Westfalen"),
+    RHEINLAND("Rheinland-Pfalz"),
+    SAARLAND("Saarland"),
+    SACHSEN("Sachsen"),
+    SACHSENANHALT("Sachsen-Anhalt"),
+    SCHLESWIG("Schleswig-Holstein"),
+    THURINGEN("Thüringen")
+}
